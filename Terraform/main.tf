3bf55d0e8a11e6145159f1e4e5564d5d60f0c86f
djmelman <tomastaptik@gmail.com>
2023-12-09 17:37:32 +0100

    Uprava

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,24 +11,6 @@ resource "openstack_networking_subnet_v2" "subnet_1" {
   ip_version = 4
 }
 
-/*resource "openstack_networking_router_v2" "router_1" {
-  name                = "my_router"
-  admin_state_up      = true
-  external_network_id = "3b3d6331-6050-497b-826f-4144382160bd"
-}
-
-
-
-resource "openstack_networking_port_v2" "port_1" {
-  name           = "port_1"
-  network_id     = openstack_networking_network_v2.main.id
-  admin_state_up = "true"
-}
-
-resource "openstack_networking_router_interface_v2" "router_interface_1" {
-  router_id = openstack_networking_router_v2.router_1.id
-  subnet_id = openstack_networking_subnet_v2.subnet_1.id
-}*/
 
 
 resource "openstack_networking_secgroup_v2" "security_group" {
@@ -147,13 +129,7 @@ resource "openstack_compute_instance_v2" "private_server" {
   network {
     name = "stud10"
   }
-  network {
-    name = "ext-net-154"
-  }
+  
   user_data = "${file("test.sh")}"
 }
 
-/*resource "openstack_compute_interface_attach_v2" "ai_1" {
-  instance_id = openstack_compute_instance_v2.public_server.id
-  network_id  = "b25e7be0-87fc-4c38-beba-d34896a71c43"
-}*/
